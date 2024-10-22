572a65b2dd4c69b15e3eb02307500431b262e39f
劉丞恩 <114967562+Joanne0119@users.noreply.github.com>
2024-10-22 15:40:50 +0800

    fix build bugs

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.split('node_modules/')[1].split('/')[0].toString(); // Chunk libraries
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 2000, // Adjust size limit to 2000 KB (for example)
+  },
 })

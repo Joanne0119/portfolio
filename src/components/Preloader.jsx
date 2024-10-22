241ff14765fb4c2fcac2e2733ccfe71cc6cea4bc
劉丞恩 <114967562+Joanne0119@users.noreply.github.com>
2024-10-23 01:24:52 +0800

    adjust

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -4,6 +4,7 @@ const Preloader = ( ) => {
   return (
     <div id='preloader' className='flex flex-col justify-center items-center'>
       <iframe className='loader' src="https://lottie.host/embed/c918fc5f-6517-48cf-9b0a-279871ed45f1/CyjVieM6rD.json"></iframe>
+      
     </div>
   )
 }
--- src/section/Navbar.jsx
+++ src/section/Navbar.jsx
@@ -74,7 +74,7 @@ const Navbar = () => {
     <header ref={navRef} className={`bg-sky-50 fixed top-0 left-0 right-0 z-50 px-8 transition-all duration-1000 ease-in-out  ${scrolled ? 'opacity-0 py-2 pointer-events-none' : 'opacity-100 py-4'}`}>
         <div className='max-w-7xl mx-auto'>
             <div className='flex justify-between'>
-                <a href='/' className='text-sky-950 font-bold text-xl hover:text-sky-600 transition-colors'>
+                <a href='#home' className='text-sky-950 font-bold text-xl hover:text-sky-600 transition-colors'>
                     Joanne
                 </a>
                 <button onClick={() => toogleMenu()} className="text-black-400 hover:text-white focus:outline-none sm:hidden flex"

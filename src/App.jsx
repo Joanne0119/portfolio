9ef8d077ba5af718cb6da70dc2482a0a0a891c95
劉丞恩 <114967562+Joanne0119@users.noreply.github.com>
2024-10-24 11:24:43 +0800

    smaller video size

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ const  App = () => {
   return (
     <main>
       {loading ? (
-          <Preloader progress={progress}/>
+          <Preloader/>
       ) : (
         <>
           <Navbar />
--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Preloader = ({progress}) => {
+const Preloader = () => {
   return (
     <div id='preloader' className='flex flex-col justify-center items-center'>
       <iframe className='loader' src="https://lottie.host/embed/c918fc5f-6517-48cf-9b0a-279871ed45f1/CyjVieM6rD.json"></iframe>
-      <p className='text-sky-500 text-xl font-generalsans font-bold'>Loading... {progress.toFixed(1)}%</p>
+      <p className='text-sky-500 text-xl font-generalsans font-bold'>Loading...</p>
     </div>
   )
 }
